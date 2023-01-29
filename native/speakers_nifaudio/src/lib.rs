2535b6bf0e5a21a5129a00509a6a1964f685f0b7
Paul Götze <paul.christoph.goetze@gmail.com>
2023-01-29 13:13:14 +0100

    Update dependencies
    * Bump Rustler to v0.27
    * Bump rodio to v0.11 (which seems to be the last version that works
      with the current implementation, because v0.12 removed the global
      audio processing, see https://github.com/RustAudio/rodio/blob/master/CHANGELOG.md#version-0120-2020-10-05)

--- native/speakers_nifaudio/src/lib.rs
+++ native/speakers_nifaudio/src/lib.rs
@@ -6,25 +6,7 @@ use std::io::BufReader;
 use std::fs::File;
 
 mod atoms {
-    rustler::rustler_atoms! {
-        atom ok;
-        //atom error;
-        //atom __true__ = "true";
-        //atom __false__ = "false";
-    }
-}
-
-rustler::rustler_export_nifs! {
-    "Elixir.Speakers.NifAudio",
-    [
-        ("add_to_queue", 1, add_to_queue),
-        ("pause", 0, pause),
-        ("resume", 0, resume),
-        ("get_queue_len", 0, get_queue_len),
-        ("get_volume", 0, get_volume),
-        ("set_volume", 1, set_volume)
-    ],
-    None
+    rustler::atoms! { ok, error }
 }
 
 lazy_static! {
@@ -35,37 +17,54 @@ lazy_static! {
     };
 }
 
-fn pause<'a>(env: Env<'a>, _args: &[Term<'a>]) -> Result<Term<'a>, Error> {
+#[rustler::nif]
+pub fn pause<'a>(env: Env<'a>) -> Result<Term<'a>, Error> {
     CURRENT_SINK.pause();
     Ok((atoms::ok()).encode(env))
 }
 
-fn resume<'a>(env: Env<'a>, _args: &[Term<'a>]) -> Result<Term<'a>, Error> {
+#[rustler::nif]
+pub fn resume<'a>(env: Env<'a>) -> Result<Term<'a>, Error> {
     CURRENT_SINK.play();
     Ok((atoms::ok()).encode(env))
 }
 
-fn get_queue_len<'a>(env: Env<'a>, _args: &[Term<'a>]) -> Result<Term<'a>, Error> {
+#[rustler::nif]
+pub fn get_queue_len<'a>(env: Env<'a>) -> Result<Term<'a>, Error> {
     let queue_len = CURRENT_SINK.len();
     Ok((atoms::ok(), queue_len).encode(env))
 }
 
-fn add_to_queue<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
-    let file_path: &str = args[0].decode()?;
-    let audio_file = File::open(file_path).unwrap();
+#[rustler::nif]
+pub fn add_to_queue<'a>(env: Env<'a>, file_path: String) -> Result<Term<'a>, Error> {
+    let file = File::open(&file_path).unwrap();
+    let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
 
-    CURRENT_SINK.append(rodio::Decoder::new(BufReader::new(audio_file)).unwrap());
+    CURRENT_SINK.append(source);
 
     Ok((atoms::ok()).encode(env))
 }
 
-fn get_volume<'a>(env: Env<'a>, _args: &[Term<'a>]) -> Result<Term<'a>, Error> {
+#[rustler::nif]
+pub fn get_volume<'a>(env: Env<'a>) -> Result<Term<'a>, Error> {
     let current_volume = CURRENT_SINK.volume();
     Ok((atoms::ok(), current_volume).encode(env))
 }
 
-fn set_volume<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
-    let new_volume: f32 = args[0].decode()?;
+#[rustler::nif]
+pub fn set_volume<'a>(env: Env<'a>, new_volume: f32) -> Result<Term<'a>, Error> {
     let new_set_volume = CURRENT_SINK.set_volume(new_volume);
     Ok((atoms::ok(), new_set_volume).encode(env))
 }
+
+rustler::init!(
+    "Elixir.Speakers.NifAudio",
+    [
+        add_to_queue,
+        pause,
+        resume,
+        get_queue_len,
+        get_volume,
+        set_volume
+    ]
+);
